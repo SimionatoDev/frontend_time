631e3772b1d2b78d9fb7f4e80b9ebebb96670c4c
Simionato <dev@simionatoauditores.com.br>
2024-10-24 09:20:59 -0300

    troca de repositorio

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
-/*
 export const environment = {
   versao: 'V 01.50 - Beta',
   data: '25/07/2023',
   production: false,
   ambiente: 'Desenvolvimento',
-  apiURL: 'https://backend-controle-time.up.railway.app/api/',
+  apiURL: 'https://simionatotimerbackend-production.up.railway.app/api/',
 };
-*/
 
+/*
 export const environment = {
   versao: 'V 01.50 - Beta',
   data: '06-02-2024',
@@ -15,3 +14,4 @@ export const environment = {
   ambiente: 'Produção',
   apiURL: 'http://localhost:3000/api/',
 };
+*/
